212976f24e86b434a1ebb5e33312afa60318dc1f
Dean <zabrahair@gmail.com>
2018-09-23 11:48:29 +0800

    1. set webapp port to 8090 2, add @bean for entrance of webapp REstApiApplication 3, add a record audio upload process controller.

---
+++ rest-api/src/main/java/meeting/record/restapi/controller/MeetingRecorderController.java
@@ -0,0 +1,13 @@
+package meeting.record.restapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeetingRecorderController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
